0ff35244717f32916f1ce7405679b02a817cdf64
MrGamz <123112118+MrGamzWasTaken@users.noreply.github.com>
2023-03-25 16:38:03 -0400

    Add files via upload

---
+++ WelcomeBot/DiscordBot.py
@@ -0,0 +1,33 @@
+import discord
+
+TOKEN = "MTA4OTIwNDExOTcyNzc3NTc2NQ.GU4qTy.um4abBkMra6WNqpJKpyNufISeFjY_cl0C-Miwk"
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print("Logged in as {0.user}".format(client))
+
+@client.event
+async def on_member_join(member):
+    await member.create_dm()
+    await member.dm_channel.send(f"Hi {member.name}, Welcome to the server!")
+
+@client.event
+async def on_message(message):
+    username = str(message.author).split("#")[0]
+    userMessage = str(message.content).lower()
+    channel = str(message.channel.name)
+    if message.author == client.user:
+        return
+
+    if message.channel.name == "commands":
+        if userMessage == "hi" or userMessage == "hello":
+            await message.channel.send(f"Hello {username}")
+            return
+        if userMessage == "bye" or userMessage == "goodbye":
+            await message.channel.send(f"Goodbye {username}")
+            return
+
+
+client.run(TOKEN)
